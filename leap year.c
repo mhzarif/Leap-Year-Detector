b65ef46275a267c37a36b3288fc46aec82cefdbd
Muhammad Hasan Zarif <mhzarif16@gmail.com>
2023-12-08 11:44:14 +0600

    Add files via upload

---
+++ leap year.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main ()
+
+{
+
+    int y, i=4;
+
+    printf("Enter any year: ");
+    scanf("%d", &y);
+
+    printf("\n");
+
+    if(y%i==0)
+        {
+            printf(">> &d is a leap year", y);
+            printf("\n");
+        }
+    else
+        {
+            printf(">> %d is not a leap year", y);
+            printf("\n");
+        }
+
+
+
+}
